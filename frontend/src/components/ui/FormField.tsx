8a03c742754782be7a46d17beec9176186d03811
loredanadelic <loredanadelic@gmail.com>
2025-07-14 22:10:11 +0200

    feat: add FormField and Input components

---
+++ frontend/src/components/ui/FormField.tsx
@@ -0,0 +1,37 @@
+// Libraries
+import * as React from "react";
+import { useController, useFormContext } from "react-hook-form";
+
+// Components
+import { TextField } from "./Input";
+
+export const FormField = ({
+  placeholder,
+  name,
+}: {
+  placeholder: string;
+  name: string;
+  type?: React.HTMLInputTypeAttribute;
+}) => {
+  const { field, fieldState } = useController({ name });
+  const { trigger } = useFormContext();
+
+  return (
+    <div className="relative flex flex-col">
+      <TextField
+        inputProps={{
+          className:
+            "h-10 mb-4 w-full focus:border-primary  text-base border-gray-300 rounded-md p-2",
+          placeholder: placeholder,
+        }}
+        onChange={(text) => {
+          field.onChange(text);
+          if (fieldState.invalid) {
+            trigger(name);
+          }
+        }}
+        value={field.value}
+      />
+    </div>
+  );
+};
---
+++ frontend/src/components/ui/Input.tsx
@@ -0,0 +1,31 @@
+// Libraries
+import * as React from "react";
+import * as ReactAria from "react-aria-components";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = ReactAria.InputProps;
+
+export const Input: React.FC<InputProps> = ({ className, ...props }) => (
+  <ReactAria.Input
+    {...props}
+    className={twMerge(
+      "h-14 w-full rounded-none border bg-white text-sm ",
+      className as string
+    )}
+  />
+);
+
+export const TextField: React.FC<
+  ReactAria.TextFieldProps & {
+    floatingLabel?: string;
+    isLoading?: boolean;
+    inputProps?: InputProps;
+  }
+> = ({ floatingLabel, isLoading, inputProps, className, ...props }) => (
+  <ReactAria.TextField
+    {...props}
+    className={twMerge("relative", className as string)}
+  >
+    <Input {...inputProps} />
+  </ReactAria.TextField>
+);
